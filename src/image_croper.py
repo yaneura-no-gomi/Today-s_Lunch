f11ec627604d1a2f53d76f4665e733d4d006f40c
Toshiya Yamaguchi <3724348-yamaguchi5@users.noreply.gitlab.com>
2020-07-18 11:43:41 +0900

    :zap: Removing lines in images and croping them

--- src/image_croper.py
+++ src/image_croper.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 from pprint import pprint
 
+VERTICAL = [0, 115, 230, 345, 460, 575, 689]
+HORIZONTAL = [0, 17, 37, 91, 141, 185, 228, 271, 315, 359]
 
 class ImageCroper:
     def __init__(self, img_path):
@@ -14,11 +16,12 @@ class ImageCroper:
         # Grayscale Conversion & Negative Positive Conversion
         img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         ret, img_thresh = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+        self.img_thresh = img_thresh
         img = cv2.bitwise_not(img_thresh)
 
         # Lines Detection by Hough Conversion
         lines = cv2.HoughLinesP(
-            img, rho=1, theta=np.pi / 360, threshold=80, minLineLength=250, maxLineGap=5
+            img, rho=1, theta=np.pi / 360, threshold=80, minLineLength=250, maxLineGap=20
         )
 
         self.lines = [line[0] for line in lines]
@@ -42,26 +45,44 @@ class ImageCroper:
 
         return removed_l
 
+    def remove_lines(self):
+        for line in self.lines:
+            x1, y1, x2, y2 = line
+            self.no_lines_img = cv2.line(self.img_thresh, (x1,y1), (x2,y2), (255,255,255), 1)
+            # cv2.imwrite("no_line.png", no_lines_img)
+
     def crop_by_frame(self):
         """
         Find the coordinates of the required intersection points
         """
 
-        # Find the coordinates of the required intersection points
-        vertical = []
-        horizontal = []
-        for line in self.lines:
-            if line[0] == line[2]:
-                # vertical line
-                vertical.append(line[0])
+        # if self.lines is not None:
+        #     # Find the coordinates of the required intersection points
+        #     vertical = []
+        #     horizontal = []
+        #     for line in self.lines:
+        #         if line[0] == line[2]:
+        #             # vertical line
+        #             vertical.append(line[0])
 
-            elif line[1] == line[3]:
-                # horizontal line
-                horizontal.append(line[1])
+        #         elif line[1] == line[3]:
+        #             # horizontal line
+        #             horizontal.append(line[1])
 
-        # Close elements are eliminated
-        vertical = self.eliminate_close_element(sorted(vertical))
-        horizontal = self.eliminate_close_element(sorted(horizontal))
+        #     # Close elements are eliminated
+        #     vertical = self.eliminate_close_element(sorted(vertical))
+        #     horizontal = self.eliminate_close_element(sorted(horizontal))
+
+        #     if vertical[0] != 0:
+        #         vertical.insert(0, 0)
+        #     if horizontal[0] != 0:
+        #         horizontal.insert(0, 0)
+
+        self.extract_lines()
+        self.remove_lines()
+
+        vertical = VERTICAL
+        horizontal = HORIZONTAL
 
         pprint(vertical)
         pprint(horizontal)
@@ -69,9 +90,9 @@ class ImageCroper:
         # Croping 
         croped_imgs = dict()
         for i, day in enumerate(["mon", "tue", "wed", "thu", "fri", "sat"]):
-            _ = self.img[:, vertical[i] : vertical[i + 1]]
+            _ = self.no_lines_img[:, vertical[i] : vertical[i + 1]]
             cv2.imwrite("result/" + day + ".png", _)
-            croped_imgs[day] = self.img[:, vertical[i] : vertical[i + 1]]
+            croped_imgs[day] = self.no_lines_img[:, vertical[i] : vertical[i + 1]]
 
         for k, v in croped_imgs.items():
             _ = []
@@ -87,10 +108,7 @@ def main():
 
     img_path = "2.jpg"
     croper = ImageCroper(img_path)
-    croper.extract_lines()
-    # croper.check_lines()
     croper.crop_by_frame()
-
 
 if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from pytesseract import pytesseract
 
 from image_croper import ImageCroper
 
+ROOT_PATH = "/opt/"
 
 def get_menu_image():
 
@@ -25,9 +26,33 @@ def get_menu_image():
 
     for i, img in enumerate(imgs):
         r = requests.get(img)
-        with open(os.path.join("..", str(i)+".jpg"),'wb') as file:
+        with open(os.path.join(ROOT_PATH, str(i)+"_2.jpg"),'wb') as file:
                 file.write(r.content)
 
+def cv2pil(image):
+    ''' OpenCV型 -> PIL型 '''
+    new_image = image.copy()
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = new_image[:, :, ::-1]
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = new_image[:, :, [2, 1, 0, 3]]
+    new_image = Image.fromarray(new_image)
+    return new_image
+
+def crop_img(img_path):
+    croper = ImageCroper(img_path)
+    croper.extract_lines()
+    croped_imgs = croper.crop_by_frame()
+
+    for k, v in croped_imgs.items():
+        pil_imgs = []
+        for cv_img in v:
+            pil_imgs.append(cv2pil(cv_img))
+        v = pil_imgs
+
+    return croped_imgs
 
 def ocr(img_path):
     path_tesseract = "/usr/bin/tesseract"
@@ -36,7 +61,10 @@ def ocr(img_path):
     
     tools = pyocr.get_available_tools()
     tool = tools[0]
-    img_org = Image.open(img_path)
+
+    croped_imgs = crop_img(img_path)
+
+    print(croped_imgs)
 
     builder = pyocr.builders.TextBuilder()
     result = tool.image_to_string(img_org, lang="jpn", builder=builder)
@@ -45,9 +73,8 @@ def ocr(img_path):
 
     
 def main():
-    # get_menu_image()
+    get_menu_image()
     # print(ocr("/opt/1.jpg"))
-    crop_by_frame("/opt/1.jpg")
 
 if __name__ == "__main__":
     main()
