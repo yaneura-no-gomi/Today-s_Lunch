c89f5e72695b62e92ff85fe9d58dd89f10ec28d2
Toshiya Yamaguchi <3724348-yamaguchi5@users.noreply.gitlab.com>
2021-04-11 20:25:17 +0900

    :zap: remove hello and add auto updating images

--- slackbot/run.py
+++ slackbot/run.py
@@ -2,9 +2,12 @@ from slackbot.bot import Bot
 from slacker import Slacker
 import slackbot_settings
 
+from ..utils.get_menu_data import get_menu_image
+
 def main():
     slack = Slacker(slackbot_settings.API_TOKEN)
-    slack.chat.post_message('lunch', 'こんにちは!', as_user=True)
+    _ = get_menu_image()
+    # slack.chat.post_message('lunch', 'こんにちは!', as_user=True)
 
     bot = Bot()
     bot.run()
