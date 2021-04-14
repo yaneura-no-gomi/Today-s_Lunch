e6b583362a70721b7fed421e8644fc690d0971a1
Toshiya Yamaguchi <3724348-yamaguchi5@users.noreply.gitlab.com>
2021-04-14 10:47:02 +0900

    :zap: update

--- slackbot/run.py
+++ slackbot/run.py
@@ -9,10 +9,11 @@ from utils.get_menu_data import get_menu_image
 def main():
     slack = Slacker(slackbot_settings.API_TOKEN)
     _ = get_menu_image()
-    # slack.chat.post_message('lunch', 'こんにちは!', as_user=True)
-
     bot = Bot()
     bot.run()
+    slack.chat.post_message('lunch', 'こんにちは!', as_user=True)
+
+    
 
 if __name__ == "__main__":
     print('starting slackbot')
